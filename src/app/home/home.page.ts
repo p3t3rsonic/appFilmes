390727cdc4a20151087e276d76a4c16bb1421a81
Pedro Passarin <pedro_passarin@hotmail.com>
2021-09-29 21:37:11 -0300

    Aplicativo quase finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async msgAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Deseja favoritar o Filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.mensagemToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async mensagemToast() {
+    const toast = await this.toastController.create({
+      message: 'Você adicionou o filme aos Favoritos.',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
+  }
 
 }
